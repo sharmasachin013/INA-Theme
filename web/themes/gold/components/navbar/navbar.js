bd7fc80d282678fc9ff68c4260e98dd0822dfdd4
sharma.sachin013 <sharma.sachin013@gmail.com>
2025-03-17 14:47:22 +0530

    navbar error fixed

--- web/themes/gold/components/navbar/navbar.js
+++ web/themes/gold/components/navbar/navbar.js
@@ -1,187 +1,111 @@
-  let menuData;
-
-
-
-
-  // Function to create a menu
-  function createMenu(menuData) {
-
-      const navContainer = document.createElement("div");
-      navContainer.classList.add("navbar-nav", "ms-auto", "py-0");
-      menuData.forEach(element => {
-          if (element.sub_menu) {
-              const navContainerSubmenu = document.createElement("div");
-              navContainerSubmenu.classList.add("nav-item", "dropdown");
-              const link = document.createElement("a")
-              link.href = element.url;
-              link.textContent = element.title;
-              link.classList.add("nav-link", "dropdown-toggle");
-              navContainerSubmenu.appendChild(link);
-              navContainer.appendChild(navContainerSubmenu);
-              const navContainerSubmenuDropDown = document.createElement("div");
-              navContainerSubmenuDropDown.classList.add("dropdown-menu", "m-0");
-              element.sub_menu.forEach(element => {
-
-                  if (element.sub_menu) {
-                      const navContainerSubmenu1 = document.createElement("div");
-                      navContainerSubmenu1.classList.add("dropdown-submenu");
-                      const link = document.createElement("a")
-                      link.href = element.url;
-                      link.textContent = element.title;
-                      link.classList.add("dropdown-item", "dropdown-toggle");
-                      navContainerSubmenu1.appendChild(link);
-                      navContainerSubmenuDropDown.appendChild(navContainerSubmenu1);
-                      const navContainerSubmenu2 = document.createElement("div");
-                      navContainerSubmenu2.classList.add("dropdown-menu");
-
-                      element.sub_menu.forEach(item => {
-                          const link = document.createElement("a")
-                          link.href = item.url;
-                          link.textContent = item.title;
-                          link.classList.add("dropdown-item");
-                          navContainerSubmenu2.appendChild(link);
-                          navContainerSubmenu1.appendChild(navContainerSubmenu2);
-
-                      });
-
-
-
-                  } else {
-                      const link = document.createElement("a")
-                      link.href = element.url;
-                      link.textContent = element.title;
-                      link.classList.add("dropdown-item");
-                      navContainerSubmenuDropDown.appendChild(link);
-                      navContainerSubmenu.appendChild(navContainerSubmenuDropDown);
-
-                  }
-              });
-
-
-          } else {
-              const link = document.createElement("a")
-              link.href = element.url;
-              link.textContent = element.title;
-              link.classList.add("nav-item", "nav-link");
-              navContainer.appendChild(link);
-
-          }
-
-      });
-
-      return navContainer;
-  }
-
-  // Append the menu to the navbar container
-  document.addEventListener("DOMContentLoaded", () => {
-      const menuContainer = document.querySelector("#navbarCollapse"); // Replace with your actual container ID
-
-      loadMenu();
-      loadSlider();
-      //  console.log(menuData);
-
-  });
-
-  const loadMenu = async () => {
-      let mainMenu = '';
-      try {
-          const endPoint = "https://varundrupaltheme.lndo.site/api/ina-menu-api/main";
-          const response = await fetch(endPoint);
-
-          if (response.status === 200) {
-              const data = await response.json();
-              const menuContainer = document.querySelector("#navbarCollapse");
-              if (menuContainer) {
-                  menuContainer.appendChild(createMenu(data));
-              }
-              
-
-
-
-
-          } else {
-              throw new Error(`Error: ${response.status}`);
-          }
-      } catch (error) {
-
-      } finally {
-
-      }
-  };
-
-  const loadSlider = async () => {
-
-      try {
-          const endPoint = "https://varundrupaltheme.lndo.site/slider";
-          const response = await fetch(endPoint);
-
-          if (response.status === 200) {
-              const data = await response.json();
-              const sliderContainer = document.querySelector("#slider");
-              if (sliderContainer) {
-                 creatSldier(data);
-              }
-
-
-
-
-          } else {
-              throw new Error(`Error: ${response.status}`);
-          }
-      } catch (error) {
-
-      }
-
-  }
-
-
-  function creatSldier(data) {
-    //  console.log(data);
-      const sliderContainer = document.querySelector("#slider");
-    //  console.log(sliderContainer);
-      let dataUpdate = '';
-
-      data.forEach((element) => {
-
-          let path = 'https://varundrupaltheme.lndo.site' + element.field_image_1;
-       //   console.log(path);
-          const sliderCon = document.createElement("div");
-          sliderCon.classList.add("header-carousel-item");
-          const img = document.createElement("img");
-          img.src = path; // Set image source
-          img.alt = "Dynamic Image"; // Set alt text
-          sliderCon.appendChild(img);
-          sliderContainer.appendChild(sliderCon);
-
-
-          //     dataUpdate += `<div class="header-carousel-item">
-          //     <img src="${path}" class='img-fluid w-100;
-          //         alt="Image">
-
-          // </div>`
+//let menuData;
+// Function to create a menu
+function createMenu(menuData, parentElement) {
+  let output = '';
+  const subMenuArr = '';
+  menuData.forEach((item) => {
+    if (item.sub_menu) {
+      output += `<div class="nav-item dropdown">
+          <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">${item.title}</a>`;
+      output += `<div class="dropdown-menu m-0">`;
+      item.sub_menu.forEach((subMenu) => {
+        const {
+          title,
+          url,
+          sub_menu
+        } = subMenu;
+        if (!sub_menu) {
+          output += `<a href="${subMenu.url}" class="dropdown-item">${subMenu.title}</a>`;
+        } else {
+          output += `<div class="dropdown-submenu">`;
+          output += `<a href="${url}" class="dropdown-item dropdown-toggle">${title}</a>`;
+          output += `<div class="dropdown-menu">`;
+          sub_menu.forEach((x) => {
+            output += `<a href="${x.url}" class="dropdown-item">${x.title}</a>`;
+          })
+          output += `</div></div>`;
+        }
       })
-    //  console.log(dataUpdate);
+      output += `</div></div>`;
+    } else {
+      output += `<a href="${item.url}" class="nav-item nav-link">${item.title}</a>`;
+    }
+  })
+  console.log(subMenuArr);
+  parentElement.innerHTML = output;
+}
+// Append the menu to the navbar container
+document.addEventListener("DOMContentLoaded", () => {
+  const menuContainer = document.querySelector("#navbarCollapse"); // Replace with your actual container ID
+  loadMenu();
+  loadSlider();
+  //  console.log(menuData);
+});
+const loadMenu = async () => {
+  let mainMenu = '';
+  try {
+    const endPoint = "https://varundrupaltheme.lndo.site/api/ina-menu-api/main";
+    const response = await fetch(endPoint);
+    if (response.status === 200) {
+      const data = await response.json();
+      const menuContainer = document.querySelector("#navbarCollapse");
+      //   console.log(data);
+      if (menuContainer) {
+        // menuContainer.appendChild(createMenu(data));
+        menuContainer.innerHTML = ""; // Clear existing menu
+        createMenu(data, menuContainer);
+      }
+    } else {
+      throw new Error(`Error: ${response.status}`);
+    }
+  } catch (error) {} finally {}
+};
+const loadSlider = async () => {
+  try {
+    const endPoint = "https://varundrupaltheme.lndo.site/slider";
+    const response = await fetch(endPoint);
+    if (response.status === 200) {
+      const data = await response.json();
+      const sliderContainer = document.querySelector("#slider");
+      if (sliderContainer) {
+        creatSldier(data);
+      }
+    } else {
+      throw new Error(`Error: ${response.status}`);
+    }
+  } catch (error) {}
+}
 
-     // sliderContainer.appendChild(dataUpdate);
-      $(".header-carousel").trigger('destroy.owl.carousel');
-      $(".header-carousel").owlCarousel({
-          animateOut: 'slideOutDown',
-          items: 1,
-          autoplay: true,
-          smartSpeed: 1000,
-          dots: true,
-          loop: true,
-          nav: true,
-          navText: [
-              '<i class="bi bi-arrow-left"></i>',
-              '<i class="bi bi-arrow-right"></i>'
-          ],
-      });
+function creatSldier(data) {
+  const sliderContainer = document.querySelector("#slider");
+  let dataUpdate = '';
+  data.forEach((element) => {
+    let path = 'https://varundrupaltheme.lndo.site' + element.field_image_1;
+    //   console.log(path);
+    const sliderCon = document.createElement("div");
+    sliderCon.classList.add("header-carousel-item");
+    const img = document.createElement("img");
+    img.src = path; // Set image source
+    img.alt = "Dynamic Image"; // Set alt text
+    sliderCon.appendChild(img);
+    sliderContainer.appendChild(sliderCon);
 
+  })
 
-  }
+  $(".header-carousel").trigger('destroy.owl.carousel');
+  $(".header-carousel").owlCarousel({
+    animateOut: 'slideOutDown',
+    items: 1,
+    autoplay: true,
+    smartSpeed: 1000,
+    dots: true,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>'
+    ],
+  });
+}
 
-
-
-
-  // })(Drupal);
+// })(Drupal);
